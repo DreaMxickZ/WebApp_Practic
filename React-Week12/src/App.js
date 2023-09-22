88261577c8390f867031db590df3c9328d8f2e6a
DreaMxickZ <zanookdeel@gmail.com>
2023-09-22 16:08:41 +0700

    Inner Css  App.js   สำหรับส่ง

--- React-Week12/src/App.js
+++ React-Week12/src/App.js
@@ -21,16 +21,108 @@ class App extends Component {
     }
 
     return (
+      <>
+      <style>
+      {`
+     .main-content {
+      background-color: #6A6968;
+      display: flex;
+      flex-direction: column;
+      align-items: center; 
+      width: 100%;
+      height: 100vh; 
+    }
+    .Title-text {
+      margin-top: 10px;
+    }
+     h1 {
+      color:#F4C919 ;
+    }
+    
+    .box-food {
+      width: 800px;
+      height: 335px;
+      background-color: bisque;
+      border-radius: 15px;
+      border: 3px solid #000000;
+    }
+    
+    .box-content {
+      margin-top: 20px;
+      margin-left: 18px;
+      margin-right: 35px;
+      
+      
+    }
+    
+    .box-content .content {
+      display: grid;
+      grid-template-columns: 1fr 0.5fr ;
+    }
+    
+    
+    .content .detail {
+      text-align: start;
+    }
+    
+    .btn-main {
+      display: flex; 
+      justify-content: center; 
+      position: relative; 
+    }
+    
+    .button-btn {
+      display: flex;
+      justify-content: center;  
+    }
+    
+    .btn-vote {
+      height: 40px;
+      margin-top: 15px;
+    }
+    
+    .add{
+      margin-right: 30px;
+      margin-left: 40px;
+    }
+    
+    .dis {
+      margin-left: 90px;
+    }
+    
+    .score {
+      order: 2;
+      margin-left: 10px;
+      margin-right: 10px;
+      background-color:#79E307 ;
+      color: #8B41E1;
+      border: 2px solid #8B41E1;
+      height: 50px;
+      padding-left: 10px;
+      padding-right: 10px;
+      border-radius: 10px;
+      font-size: 22px;
+      font-weight: bold;
+      display: flex; 
+      justify-content: center; 
+      align-items: center; 
+      position: absolute;
+      top: 50%; 
+      left: 50%; 
+      transform: translate(-50%, -50%); 
+      
+    }
+      `}
+      </style>
       <div className="App">
-        <div className="main-content">
-          <h1>โหวตอาหาร</h1>
-            <Boxfood foodtype={menu1.foodtype} menu={menu1.menu} detail={menu1.detail} imagelocate={menu1.imagelocate} />
-            <Boxfood foodtype={menu2.foodtype} menu={menu2.menu} detail={menu2.detail} imagelocate={menu2.imagelocate} />
-
-        </div>
-        
-        
-      </div>
+         <div className="main-content">
+           <h1>โหวตอาหาร</h1>
+             <Boxfood foodtype={menu1.foodtype} menu={menu1.menu} detail={menu1.detail} imagelocate={menu1.imagelocate} />
+             <Boxfood foodtype={menu2.foodtype} menu={menu2.menu} detail={menu2.detail} imagelocate={menu2.imagelocate} />
+         </div>
+       </div>
+    </>
+    
     );
   }
 }
@@ -46,6 +138,8 @@ class Boxfood extends Component {
   render() {
     const {foodtype, menu, detail, imagelocate} = this.props;
     const {result} = this.state;
+
+   
     return (
       <div className="box-food">
                   <div className="box-content">
@@ -60,14 +154,10 @@ class Boxfood extends Component {
                       </div>
                     </div>
                     
-                    <div className="btn-main">
-                     
-                      
 
+                    <div className="btn-main">
                       <Score ScoreCallback = {this.handleCallback}/>
                       <div class="score">{result}</div>
-                     
-                      
                     </div>
                   
 
@@ -80,10 +170,12 @@ class Boxfood extends Component {
 
 
 
+
 class Score extends Component {
   state = {
     count: 0,
   }
+
 
   Addvote = () => {  
     if (this.state.count >= 10) {
@@ -104,23 +196,23 @@ class Score extends Component {
       this.props.ScoreCallback(newCount === 0 ? 'MIN' : newCount);
     }
   }
+
   
 
+
   render() {
+    
     return (
-      <div className="button-btn">
-      <button onClick={this.Addvote} className="btn-vote add">click  to vote</button>
-      <button onClick={this.Disvote} className="btn-vote dis">click  to unvote</button>
-      </div>
-      
-
+      <div className="">
+      <button onClick={this.Addvote} className="btn-vote add" style={this.buttonStyleadd}>click to vote</button>
+      <button onClick={this.Disvote} className="btn-vote dis" style={this.buttonStyledis}>click to unvote</button>
+    </div>
     );
-
   }
 }
+
+
 
 export default App;
 
 
-  // <button onClick={this.Addvote} className="btn-vote">Click Me</button>
-      // <button onClick={this.Disvote} className="btn-vote dis">Click Me</button>
