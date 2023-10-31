f3ab83373ec7387e70b1e532474a4d175ecee732
DreaMxickZ <zanookdeel@gmail.com>
2023-10-31 16:43:49 +0700

    Finish Exaple

---
+++ MVC-Week14/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Ex7.Models;
+
+namespace Ex7.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    
+
+    public IActionResult Index(string rangeoftower, string fname , string lname , string txtans , string email)
+    {
+        info person = new info();
+        person.Rangeoftower = rangeoftower;
+        person.FName = fname;
+        person.LName = lname;
+        person.Txtans = txtans;
+        person.Email = email;
+
+        return View(person);
+    }
+
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ MVC-Week14/Models/ErrorViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex7.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+   
+}
+
+public class info
+{
+    public string FName { get; set; }
+    public string LName { get; set; }
+    public string Rangeoftower { get; set; }
+    public string Txtans { get; set; }
+    public string Email { get; set; }
+}
+
